ba1376b406ee8d83a4c650977aa9c15a412efde3
KhaledMohammed <khaled.1m2h3o@gmail.com>
2022-12-22 18:27:59 +0200

    commit 27 adjustment dashboard

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -17,6 +17,7 @@ class UserController extends Controller
      */
     public function index()
     {
+        //test
         if(auth()->user()->role_id == 1){
             $users = User::where('role_id',2)->orderBy('id','DESC')->paginate(5);
 
